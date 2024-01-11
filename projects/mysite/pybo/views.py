6dff6d92efb754cdd7905117f2c979bc19c66fce
chowchow0048 <suess98@naver.com>
2024-01-11 14:56:08 +0900

    model CRUD, URL namespace

--- projects/mysite/pybo/views.py
+++ projects/mysite/pybo/views.py
@@ -1,6 +1,23 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404, redirect
+from django.utils import timezone
+from .models import Question
 
 
 def index(req):
-    return HttpResponse("Hello, welcome to pybo.")
+    question_list = Question.objects.order_by("-create_date")
+    context = {"question_list": question_list}
+    return render(req, "pybo/question_list.html", context)
+
+
+def detail(req, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    context = {"question": question}
+    return render(req, "pybo/question_detail.html", context)
+
+
+def answer_create(req, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    question.answer_set.create(
+        content=req.POST.get("content"), create_date=timezone.now()
+    )
+    return redirect("pybo:detail", question_id=question_id)
